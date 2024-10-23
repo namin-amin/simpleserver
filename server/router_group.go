7335c4b998cc22b379f0bfeaf8df4f8134e72891
namin-amin <navaneesh185@gmail.com>
2024-10-23 23:59:55 +0530

    add methods to routergroup

--- server/router_group.go
+++ server/router_group.go
@@ -39,3 +39,27 @@ func (rg *RouterGroup) HandleFunc(pattern string, handler RouteHandler, middlewa
 
 	rg.s.HandleFunc(rg.prefix+pattern, handler, middleware...)
 }
+
+func (s *RouterGroup) GET(route string, hanler RouteHandler, middleware ...MiddlewareHandler)  {
+	s.HandleFunc(fmt.Sprintf("GET %s",route),hanler,middleware...)
+}
+
+func (s *RouterGroup) POST(route string, hanler RouteHandler, middleware ...MiddlewareHandler)  {
+	s.HandleFunc(fmt.Sprintf("POST %s",route),hanler,middleware...)
+}
+
+func (s *RouterGroup) PATCH(route string, hanler RouteHandler, middleware ...MiddlewareHandler)  {
+	s.HandleFunc(fmt.Sprintf("PATCH %s",route),hanler,middleware...)
+}
+
+func (s *RouterGroup) PUT(route string, hanler RouteHandler, middleware ...MiddlewareHandler)  {
+	s.HandleFunc(fmt.Sprintf("PUT %s",route),hanler,middleware...)
+}
+
+func (s *RouterGroup) DELETE(route string, hanler RouteHandler, middleware ...MiddlewareHandler)  {
+	s.HandleFunc(fmt.Sprintf("DELETE %s",route),hanler,middleware...)
+}
+
+func (s *RouterGroup) HEAD(route string, hanler RouteHandler, middleware ...MiddlewareHandler)  {
+	s.HandleFunc(fmt.Sprintf("HEAD %s",route),hanler,middleware...)
+}
