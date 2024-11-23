47f957cb37219bf86f90d9c0d73bd721c1a8f612
namin-amin <navaneesh185@gmail.com>
2024-11-23 17:48:52 +0530

    correct migrations folder path

--- data/migration/migrator.go
+++ data/migration/migrator.go
@@ -11,7 +11,7 @@ import (
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
-const migrationFilesPath = "./migrations/*.sql"
+const migrationFilesPath = "migrations"
 
 type DBType float64
 type MigrationDirection float64
@@ -28,21 +28,7 @@ const (
 
 // TODO maybe convert this to something like a builder
 func DoGenericMigration(fileSystem fs.FS, dbName string, db *sql.DB, dbType DBType, direction MigrationDirection) error {
-	d, err := iofs.New(fileSystem, migrationFilesPath)
-
-	if err != nil {
-		return err
-	}
-
-	var dri database.Driver
-
-	if dbType == POSTGRESS {
-		dri, _ = postgres.WithInstance(db, &postgres.Config{})
-	} else if dbType == SQLITE3 {
-		dri, _ = sqlite3.WithInstance(db, &sqlite3.Config{})
-	}
-
-	m, err := migrate.NewWithInstance("simpleserer_migrator", d, dbName, dri)
+	m, err := GetMigrator(fileSystem, dbName, db, dbType)
 
 	if err != nil {
 		return err
@@ -50,7 +36,6 @@ func DoGenericMigration(fileSystem fs.FS, dbName string, db *sql.DB, dbType DBTy
 
 	if direction == DOWN {
 		return m.Down()
-
 	}
 	return m.Up()
 }
