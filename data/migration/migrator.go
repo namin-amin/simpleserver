b18a7321aba2afbbee528bba5342fec51be672a9
namin-amin <navaneesh185@gmail.com>
2024-11-23 15:03:08 +0530

    basic migrator

--- data/migration/migrator.go
+++ data/migration/migrator.go
@@ -1,1 +1,74 @@
 package migration
+
+import (
+	"database/sql"
+	"io/fs"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/database/sqlite3"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+)
+
+const migrationFilesPath = "./migrations/*.sql"
+
+type DBType float64
+type MigrationDirection float64
+
+const (
+	POSTGRESS DBType = iota
+	SQLITE3
+)
+
+const (
+	UP MigrationDirection = iota
+	DOWN
+)
+
+// TODO maybe convert this to something like a builder
+func DoGenericMigration(fileSystem fs.FS, dbName string, db *sql.DB, dbType DBType, direction MigrationDirection) error {
+	d, err := iofs.New(fileSystem, migrationFilesPath)
+
+	if err != nil {
+		return err
+	}
+
+	var dri database.Driver
+
+	if dbType == POSTGRESS {
+		dri, _ = postgres.WithInstance(db, &postgres.Config{})
+	} else if dbType == SQLITE3 {
+		dri, _ = sqlite3.WithInstance(db, &sqlite3.Config{})
+	}
+
+	m, err := migrate.NewWithInstance("simpleserer_migrator", d, dbName, dri)
+
+	if err != nil {
+		return err
+	}
+
+	if direction == DOWN {
+		return m.Down()
+
+	}
+	return m.Up()
+}
+
+func GetMigrator(fileSystem fs.FS, dbName string, db *sql.DB, dbType DBType) (*migrate.Migrate, error) {
+	d, err := iofs.New(fileSystem, migrationFilesPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var dri database.Driver
+
+	if dbType == POSTGRESS {
+		dri, _ = postgres.WithInstance(db, &postgres.Config{})
+	} else if dbType == SQLITE3 {
+		dri, _ = sqlite3.WithInstance(db, &sqlite3.Config{})
+	}
+
+	return migrate.NewWithInstance("simpleserer_migrator", d, dbName, dri)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/namin-amin/simpleserver
 go 1.23.1
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.18.1
 	github.com/google/uuid v1.6.0
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.9.0
@@ -11,8 +12,12 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
@@ -26,8 +31,8 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
